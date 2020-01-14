9b81996077f9b5c9247fc9b482d149c592c7223d
James Day <jday6@mail.sfsu.edu>
2020-01-14 13:33:43 -0800

    basic layout done

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var hbs = require('express-handlebars')
+var path = require('path')
+var morgan = require('morgan')
+
+var app = express()
+
+app.use(morgan('dev'))
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({extended: true }))
+
+app.use(express.static(__dirname + '/public'));
+
+app.engine('hbs', hbs({
+  extname:'hbs',
+  defaultLayout:'layout',
+  layoutsDir:__dirname + '/views/layouts/'
+}));
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs');
+
+app.get('/', function(req,res){
+    res.render('home')
+})
+
+app.listen(3000, function(){
+    console.log('listening on port 3000...')
+})
