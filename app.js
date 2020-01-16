16dd0c3cd05361a3e8a3012a175094281edad98a
James Day <jday6@mail.sfsu.edu>
2020-01-16 12:52:44 -0800

    Set up seperate routes folder

--- app.js
+++ app.js
@@ -21,9 +21,8 @@ app.engine('hbs', hbs({
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs');
 
-app.get('/', function(req,res){
-    res.render('home')
-})
+var routes = require('./routes/routes.js')
+app.use('/', routes)
 
 app.listen(3000, function(){
     console.log('listening on port 3000...')
