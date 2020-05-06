d247a66d396684fdbd0784f76d2cc5a65c8e0a1f
James Day <jday6@mail.sfsu.edu>
2020-05-05 19:40:22 -0700

    added a tab switcher for possible ui design change, also added function to make input fields all caps lock(helpful for license input)

--- public/javascript/database.js
+++ public/javascript/database.js
@@ -1,18 +1,50 @@
-// document.querySelector("#databaseForm").addEventListener("submit", getData);
+// FORM INSERT 
+document.querySelector(".test_form").addEventListener("submit", getData);
+function getData(e){
+    const data = document.querySelector(".test_data").value;
 
-// function getData(e){
-//     const data = document.querySelector(".data").value;
+    console.log(data);
 
-//     console.log(data);
+    e.preventDefault();
+}
 
-//     e.preventDefault();
-// }
-
-
+// FORM DROP DOWN SELECTOR
 // used helpful answer from Simon on stackoverflow https://stackoverflow.com/questions/11959889/show-hide-different-forms-based-on-a-option-selected
 // jquery to select different forms from dropdown menu to select proper fields to insnert per table 
 $("#insert_table_select").on("change", function() {
+    // show selected table and hide all others
     $("#" + $(this).val()).show().siblings().hide()
+    // show the table drop down form 
     $("#insert_table_form").show()  
 })
 
+// TAB SWITCHER
+function openTab(event, tabID) {
+    // hide all class tab_content 
+    tab_content = document.getElementsByClassName('tab_content')
+    for (i = 0; i < tab_content.length; i++) {
+        tab_content[i].style.display = 'none'
+    }
+
+    // remove is-active from class tab_links
+    tab_links = document.getElementsByClassName('tab_links')
+    for (i = 0; i < tab_links.length; i++) {
+        tab_links[i].className = tab_links[i].className.replace(' is-active',' ')
+        
+    }
+
+    // show current tab by adding is-active to the tab_link
+    document.getElementById(tabID).style.display = 'block'
+    event.currentTarget.className += " is-active"
+}
+
+
+// FORCING INPUT TO CAPS
+// help from https://www.the-art-of-web.com/html/input-field-uppercase/
+document.getElementById('caps').addEventListener('input', forceUpperCase, false); // changed to on input so theres no delay
+function forceUpperCase(event){
+    var start = event.target.selectionStart
+    var end = event.target.selectionEnd
+    event.target.value = event.target.value.toUpperCase()
+    event.target.setSelectionRange(start, end)
+}
